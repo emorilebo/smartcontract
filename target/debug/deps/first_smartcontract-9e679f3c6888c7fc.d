9105947d10aec26daa06391dfde5eadc1e05ce9c
emorilebo <emorylebo@gmail.com>
2022-02-16 18:36:01 +0100

    First commit

---
+++ target/debug/deps/first_smartcontract-9e679f3c6888c7fc.d
@@ -0,0 +1,5 @@
+c:\Users\emory\Documents\RustProjects\first-smartcontract\target\debug\deps\first_smartcontract-9e679f3c6888c7fc.rmeta: src\main.rs
+
+c:\Users\emory\Documents\RustProjects\first-smartcontract\target\debug\deps\first_smartcontract-9e679f3c6888c7fc.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/first_smartcontract-e65817a14825be78.d
@@ -0,0 +1,5 @@
+c:\Users\emory\Documents\RustProjects\first-smartcontract\target\debug\deps\first_smartcontract-e65817a14825be78.rmeta: src\main.rs
+
+c:\Users\emory\Documents\RustProjects\first-smartcontract\target\debug\deps\first_smartcontract-e65817a14825be78.d: src\main.rs
+
+src\main.rs:
